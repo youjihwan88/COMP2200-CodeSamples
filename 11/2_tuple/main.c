4e23c52be7e6cd498ffb6390e27d3c4b30c2381d
JiHwan You <youjihwan88@gmail.com>
2025-04-11 23:42:33 +0900

    feat: Add week 11 samples.

---
+++ 11/2_tuple/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* id(int), "name"(const char*), hp(int) */
+#define MONSTER_DATA                \
+    MONSTER_ENTRY(0, "pope", 100)   \
+    MONSTER_ENTRY(1, "big rat", 30) \
+    MONSTER_ENTRY(2, "mama", 255)   \
+    MONSTER_ENTRY(3, "dragon", 300000)
+
+int main(void)
+{
+    size_t i;
+
+    int ids[] = {
+#define MONSTER_ENTRY(id, name, hp) id,
+        MONSTER_DATA
+#undef MONSTER_ENTRY
+    };
+
+    const char *names[] = {
+#define MONSTER_ENTRY(id, name, hp) name,
+        MONSTER_DATA
+#undef MONSTER_ENTRY
+    };
+
+    int healths[] = {
+#define MONSTER_ENTRY(id, name, hp) hp,
+        MONSTER_DATA
+#undef MONSTER_ENTRY
+    };
+
+    for (i = 0; i < sizeof(ids) / sizeof(int); ++i)
+    {
+        printf("%3d %6d %s\n",
+               ids[i], healths[i], names[i]);
+    }
+
+    return 0;
+}
---
+++ 11/3_getter/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* (type, name) */
+#define MONSTER_STRUCT                 \
+    MONSTER_MEMBER(int, id)            \
+    MONSTER_MEMBER(const char *, name) \
+    MONSTER_MEMBER(int, hp)
+
+typedef struct
+{
+#define MONSTER_MEMBER(type, name) type name;
+    MONSTER_STRUCT
+#undef MONSTER_MEMBER
+} monster_t;
+
+#define MONSTER_MEMBER(type, name)            \
+    type get_mob_##name(const monster_t *mob) \
+    {                                         \
+        return mob->name;                     \
+    }
+
+MONSTER_STRUCT
+
+#undef MONSTER_MEMBER
+
+int main(void)
+{
+    monster_t mob;
+    mob.id = 0;
+    mob.name = "Pope Mob";
+    mob.hp = 10001;
+
+    printf("%3d %6d %s\n",
+           get_mob_id(&mob),
+           get_mob_hp(&mob),
+           get_mob_name(&mob));
+
+    return 0;
+}
